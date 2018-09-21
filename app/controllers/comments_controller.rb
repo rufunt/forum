2b7bbe434082468ef3c767f3bfae7ebc3be5276e
rufunt <jazzoo@mail.ru>
2018-09-21 18:36:01 +0000

    Add Comments Model and Controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  
+  def create
+    @message = Message.find(params[:message_id])
+    @comment = @message.comments.create(comment_params)
+    @comment.user_id = current_user.user_id
+    
+    if @comment.save
+      redirect_to message_path(@message)
+    else
+      render 'new'
+    end
+  end
+end
